61a70cba045c3e97ce8c7ec63b36989afeacb760
jkarenko <juho.karenko@gmail.com>
2024-03-16 05:43:21 +0200

    Refactor code structure and add color logging
    - Move main script to `illustrator/__main__.py` for better package structure
    - Introduce `colorlog` for enhanced logging with color support
    - Add `MODEL` and `TEMPERATURE` constants for OpenAI API usage
    - Update argument parsing and logging setup to utilize new constants and color logging
    - Include `poetry` for dependency management and project setup

--- illustrator/__main__.py
+++ illustrator/__main__.py
@@ -6,6 +6,7 @@ from random import choice
 from openai import OpenAI
 import logging
 import csv
+from colorlog import ColoredFormatter
 
 
 def encode_image(image_path):
@@ -15,6 +16,8 @@ def encode_image(image_path):
 
 
 TEST = "I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS:\n"
+MODEL = "gpt-4-vision-preview"
+TEMPERATURE = 0.0
 
 with open("styles.txt", "r") as f:
     reader = csv.reader(f)
@@ -26,80 +29,51 @@ client = OpenAI()
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--scene", default="", help="Scene description")
-parser.add_argument(
-    "--style",
-    nargs="?",
-    default="fitting the scene",
-    help="Style description, e.g. " + ", ".join(STYLE_LIST),
-)
-parser.add_argument(
-    "--dalle",
-    choices=["2", "3"],
-    default="3",
-    help="Choose Dall-e version, either '2' or '3'",
-)
-parser.add_argument(
-    "--optimized",
-    action="store_true",
-    default=False,
-    help="Create an additional optimized prompt for Dall-e",
-)
-parser.add_argument(
-    "--environment", type=str, help="Environment override, e.g. 'a fantasy world'"
-)
-parser.add_argument(
-    "--reference-image",
-    type=str,
-    default="",
-    help="Reference image for the illustration",
-)
-parser.add_argument(
-    "--debug", action="store_true", default=False, help="Enable debug mode"
-)
+parser.add_argument("--style", nargs="?", default="fitting the scene", help="Style description, e.g. " + ", ".join(STYLE_LIST))
+parser.add_argument("--dalle", choices=["2", "3"], default="3", help="Choose Dall-e version, either '2' or '3'")
+parser.add_argument("--optimized", action="store_true", default=False, help="Create an additional optimized prompt for Dall-e")
+parser.add_argument("--environment", type=str, help="Environment override, e.g. 'a fantasy world'")
+parser.add_argument("--reference-image", type=str, default="", help="Reference image for the illustration")
+parser.add_argument("--debug", action="store_true", default=False, help="Enable debug mode")
+parser.add_argument("--info", action="store_true", default=False, help="Print the prompt")
 
 visual_group = parser.add_argument_group("Image to text settings")
 visual_group.add_argument("--image-url", help="Use image at URL as scene descriptor")
-visual_group.add_argument("--image-file", help="Use image file as scene descriptor")
-visual_group.add_argument(
-    "--image-quality",
-    choices=["high", "low"],
-    default="auto",
-    help="Image quality used during image to text, either 'high' or 'low'",
-)
+visual_group.add_argument("--image-path", help="Use image file as scene descriptor")
+visual_group.add_argument("--image-quality", choices=["high", "low"], default="auto", help="Image quality used during image to text, either 'high' or 'low'", )
 
 # Create dalle-3 group
 dalle_3_group = parser.add_argument_group("Dall-e 3 specific settings")
-dalle_3_group.add_argument(
-    "--hd",
-    action="store_true",
-    default=False,
-    help="High definition mode for more detailed images",
-)
-dalle_3_group.add_argument(
-    "--detail",
-    choices=["vivid", "natural"],
-    default="vivid",
-    help="Detail level, either 'vivid' or 'natural'",
-)
+dalle_3_group.add_argument("--hd", action="store_true", default=False, help="High definition mode for more detailed images")
+dalle_3_group.add_argument("--detail", choices=["vivid", "natural"], default="vivid", help="Detail level, either 'vivid' or 'natural'")
 
-parser.add_argument(
-    "--size",
-    choices=["1", "2", "3"],
-    default="3",
-    help="1=landscape (1792x1024) or small (256x256), 2=portrait (1024x1792) or medium (512x512), 3=square (1024x1024), default=3",
-)
-parser.add_argument(
-    "--no-test", action="store_true", help="Disable test mode prompt hack"
-)
+parser.add_argument("--size", choices=["1", "2", "3"], default="3", help="1=landscape (1792x1024) or small (256x256), 2=portrait (1024x1792) or medium (512x512), 3=square (1024x1024), default=3")
+parser.add_argument("--no-test", action="store_true", help="Disable test mode prompt hack")
 args = parser.parse_args()
 
-logging.basicConfig(
-    level=logging.DEBUG if args.debug else logging.INFO,
-    format="%(asctime)s %(levelname)s: %(message)s",
+
+log_colors = {
+    'DEBUG': 'cyan',
+    'INFO': 'green',
+    'WARNING': 'yellow',
+    'ERROR': 'red',
+    'CRITICAL': 'red,bg_white',
+}
+
+formatter = ColoredFormatter(
+    "%(asctime)s%(log_color)s %(levelname)s%(reset)s:\n%(message)s\n",
+    log_colors=log_colors
 )
 
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+
+logger = logging.getLogger()
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG if args.debug else logging.INFO)
+
 # Check for mutual exclusivity
-if (args.image_url and args.image_file) or (args.dalle == "2" and args.hd):
+if (args.image_url and args.image_path) or (args.dalle == "2" and args.hd):
     parser.error("Mutually exclusive arguments provided.")
 
 if args.style.lower() == "random":
@@ -122,7 +96,7 @@ if args.size not in sizes_dalle_3 and args.size not in sizes_dalle_2:
     parser.error("Invalid --size argument. Please choose '1', '2', or '3'.")
 
 sizes = sizes_dalle_3 if args.dalle == "3" else sizes_dalle_2
-encoded_image = encode_image(args.image_file) if args.image_file else None
+encoded_image = encode_image(args.image_path) if args.image_path else None
 image_url = (
     f"data:image/jpeg;base64,{encoded_image}" if encoded_image else args.image_url
 )
@@ -141,7 +115,8 @@ reference_image_url = (
 
 def describe_image(image_url=None):
     response = client.chat.completions.create(
-        model="gpt-4-vision-preview",
+        model=MODEL,
+        temperature=TEMPERATURE,
         messages=[
             {
                 "role": "user",
@@ -160,14 +135,15 @@ def describe_image(image_url=None):
                 ],
             }
         ],
-        max_tokens=1000,
+        max_tokens=4096,
     )
     return response.choices[0].message.content
 
 
 def describe_style(image_url=None):
     response = client.chat.completions.create(
-        model="gpt-4-vision-preview",
+        model=MODEL,
+        temperature=TEMPERATURE,
         messages=[
             {
                 "role": "user",
@@ -186,7 +162,7 @@ def describe_style(image_url=None):
                 ],
             }
         ],
-        max_tokens=1000,
+        max_tokens=4096,
     )
     return response.choices[0].message.content
 
@@ -194,53 +170,60 @@ def describe_style(image_url=None):
 image_description = None
 reference_description = None
 
-if args.image_url or args.image_file:
+if args.image_url or args.image_path:
     print("Describing image...")
     image_description = describe_image(image_url=image_url)
+    logging.info(f"Image description: {image_description}")
 
 if args.reference_image:
     print("Describing reference image...")
     reference_description = describe_style(image_url=reference_image_url)
+    logging.info(f"Reference description: {reference_description}")
 
 environment_description = None
 
 if args.environment:
     print("Describing override environment...")
     response = client.chat.completions.create(
-        model="gpt-4-1106-preview",
+        model=MODEL,
         messages=[
             {
                 "role": "user",
                 "content": f"Describe the visual style of {args.environment} including but not limited to art, camera angle, view point etc",
             }
         ],
-        max_tokens=1000,
+        max_tokens=4096,
     )
     environment_description = response.choices[0].message.content
+    logging.info(f"Environment description: {environment_description}")
+
 if not environment_description and reference_description:
     environment_description = reference_description
 
 if environment_description:
     print("Re-describing scene with override environment...")
     response = client.chat.completions.create(
-        model="gpt-4-turbo-preview",
+        model=MODEL,
+        temperature=TEMPERATURE,
         messages=[
             {
                 "role": "user",
                 "content": f"Environment:\n{environment_description}\n\nUsing the above description revise the following scene:\n{image_description}. Start immediately with the description, do not add any additional text, do not ask questions, do not explain what changed and only change the backdrop and leave all action and framing of characters intact.",
             }
         ],
-        max_tokens=1000,
+        max_tokens=4096,
     )
     image_description = response.choices[0].message.content
+    logging.info(f"Revised image description: {image_description}")
 
 
-scene_description = f"\nOverview:\n{args.scene}\nDetails:\n{image_description}"
+scene_description = f"\n{f'Overview: {args.scene}' or ''}\n{f'Environment: {args.environment}' or ''}\nDetails:\n{image_description}"
 
 
 def optimize_prompt(scene_description, style):
     completion = client.chat.completions.create(
-        model="gpt-4-1106-preview",
+        model=MODEL,
+        temperature=TEMPERATURE,
         messages=[
             {
                 "role": "system",
@@ -248,22 +231,22 @@ def optimize_prompt(scene_description, style):
             },
             {
                 "role": "user",
-                "content": f"Describe this creation so DALL-E 3 can recreate it accurately as a {style} style image: {scene_description}. More emphasis on overview if provided. Only use visual facts, opinions and describe it as if it was created in the style of {style}.",
+                "content": f"Describe this creation so DALL-E 3 can recreate it accurately as a {style} style image: {scene_description}. Make sure the notable facial features of any characters are retained and adapted into the environment {f'of {args.environment}' if args.environment else ''}. Only use visual facts, cues, opinions and describe them as if it was created in the style of {style}. Emphasize the visual constraints of {args.style or 'the appropriate style'} and {args.environment or 'the environment'}.",
             },
         ],
+        max_tokens=4096,
     )
-    return completion.choices[0].message.contentﬂﬂﬂﬂ
+    optimized_prompt = completion.choices[0].message.content
+    logging.info(f"Optimized image prompt: {optimized_prompt}")
+    return optimized_prompt
 
+scene_prompt = f"\nScene:\n{scene_description}\n\nStyle:\n{args.style}\n\n{args.scene}"
+logging.info(f"Image prompt: {scene_prompt}")
 
 if args.optimized:
-    scene_prompt = optimize_prompt(scene_description, args.style)
-else:
-    scene_prompt = (
-        f"\nScene:\n{scene_description}\n\nStyle:\n{args.style}\n\n{args.scene}"
-    )
+    scene_prompt = optimize_prompt(f"{scene_description} {args.scene}", args.style)
+
 scene_prompt = scene_prompt if args.no_test else TEST + scene_prompt
-if args.debug:
-    print(scene_prompt)
 
 
 def handle_moderation_result(moderation):
